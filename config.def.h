bc773a7c99a4d457f3b136e32c7e890a37aa4b5b
Alikku <4l1kku@gmail.com>
2022-09-27 10:32:51 +0300

    change kill client hotkey

--- config.def.h
+++ config.def.h
@@ -1026,7 +1026,7 @@ static const Key keys[] = {
 	#if BAR_WINTITLEACTIONS_PATCH
 	{ MODKEY|ControlMask,           XK_z,          showhideclient,         {0} },
 	#endif // BAR_WINTITLEACTIONS_PATCH
-	{ MODKEY|ShiftMask,             XK_c,          killclient,             {0} },
+	{ MODKEY,                       XK_q,          killclient,             {0} },
 	#if KILLUNSEL_PATCH
 	{ MODKEY|ShiftMask,             XK_x,          killunsel,              {0} },
 	#endif // KILLUNSEL_PATCH
